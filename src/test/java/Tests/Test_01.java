a648179491ec8d0ae9bb1703e507fc05d2f276cc
yasko123 <jasko@i.ua>
2015-10-25 17:16:16 +0200

    for commit

--- src/test/java/Tests/Test_01.java
+++ src/test/java/Tests/Test_01.java
@@ -5,6 +5,7 @@ import core.BaseTest;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 import ui_Layer.HomePage;
+import ui_Layer.UploadFilePopUp;
 import ui_Layer.Wall;
 
 /**
@@ -14,6 +15,7 @@ public class Test_01 extends BaseTest{
 
     private HomePage homePage;
     private Wall wall;
+    private UploadFilePopUp uploadFilePopUp;
 
 
     @Test(dataProvider = "Test_01", dataProviderClass = DataDrivenClass.class)
@@ -21,6 +23,8 @@ public class Test_01 extends BaseTest{
     public void test_01(String randomText) {
         homePage = new HomePage();
         wall = new Wall();
+        uploadFilePopUp = new UploadFilePopUp();
+
 
         Reporter.log("<b>TEST CASE name: Создать пост на стене</b>");
 
